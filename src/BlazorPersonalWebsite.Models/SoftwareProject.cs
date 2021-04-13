dd3d273c72f9f3954fca21d3c1f26c86aefcfbc5
PeterW1992 <williamsp1992@gmail.com>
2021-04-13 22:31:28 +0100

    Added initial pages
    Software Projects
    Woodwork Projects

---
+++ src/BlazorPersonalWebsite.Models/SoftwareProject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BlazorPersonalWebsite.Models
+{
+    public class SoftwareProject
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime DateCreated { get; set; }
+    }
+}
